ff31a674d91826f4fa47a12fee2a0b3972d844a6
miles <miles_xjn@163.com>
2017-07-02 14:26:13 +0800

    新增获取菜单接口

--- serve/model.js
+++ serve/model.js
@@ -9,9 +9,20 @@ const partnerStoreSchema = new mongoose.Schema({
   storeId: String,
 })
 
+const storeMenuSchema = new mongoose.Schema({
+  name: String,
+  abstract: String,
+  price: Number,
+  photo: String,
+  marking: String,
+  sales: Number,
+  comment: Array
+})
+
 const Models = {
   AppUser: mongoose.model('appUser', appUserSchema, 'appUser'),
-  partnerStore: mongoose.model('partnerStore', partnerStoreSchema, 'partnerStore')
+  partnerStore: mongoose.model('partnerStore', partnerStoreSchema, 'partnerStore'),
+  storeMenu: mongoose.model('menu', storeMenuSchema, 'menu')
 }
 
 module.exports = Models
